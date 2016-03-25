cc0380dcf5d15a3ee33c5d7a072e2909ca6fe970
mengli <dream83619@hotmail.com>
2016-03-25 14:12:16 +0800

    commit

--- app/src/main/java/com/xiaoheifamily/bookstore/binding/recyclerview/RecyclerViewBindingAdapters.java
+++ app/src/main/java/com/xiaoheifamily/bookstore/binding/recyclerview/RecyclerViewBindingAdapters.java
@@ -2,18 +2,73 @@ package com.xiaoheifamily.bookstore.binding.recyclerview;
 
 import android.databinding.BindingAdapter;
 import android.databinding.ObservableList;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.xiaoheifamily.bookstore.adapter.ObservableRecyclerViewAdapter;
+import com.xiaoheifamily.bookstore.functional.Action;
 
 @SuppressWarnings("unused")
 public class RecyclerViewBindingAdapters {
+
+    public interface LoadMoreListener {
+        void onLoadMore(RecyclerView recyclerView, Action loadFinished);
+    }
+
+    public static abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnScrollListener {
+
+        private final int visibleThreshold;
+        private boolean loading;
+        private LinearLayoutManager layoutManager;
+
+        public EndlessRecyclerViewScrollListener(int visibleThreshold) {
+            this.visibleThreshold = visibleThreshold;
+        }
+
+        public void setLoadFinished() {
+            loading = false;
+        }
+
+        @Override
+        public void onScrolled(RecyclerView view, int dx, int dy) {
+
+            if (layoutManager == null) {
+                layoutManager = (LinearLayoutManager) view.getLayoutManager();
+            }
+
+            int firstVisibleItem = layoutManager.findFirstVisibleItemPosition();
+            int visibleItemCount = view.getChildCount();
+            int totalItemCount = layoutManager.getItemCount();
+
+            if (!loading && (firstVisibleItem + visibleThreshold + visibleItemCount) >= totalItemCount) {
+                loading = true;
+                onLoadMore();
+            }
+        }
+
+        protected abstract void onLoadMore();
+    }
 
     @BindingAdapter("layoutManager")
     public static void setLayoutManager(RecyclerView recyclerView,
                                         LayoutManagers.LayoutManagerFactory layoutManagerFactory) {
 
         recyclerView.setLayoutManager(layoutManagerFactory.create(recyclerView));
+    }
+
+    @BindingAdapter(value = {"loadMore", "visibleThreshold"}, requireAll = false)
+    public static void setLoadMore(RecyclerView recyclerView, LoadMoreListener listener, Integer visibleThreshold) {
+
+        if (visibleThreshold == null) {
+            visibleThreshold = 0;
+        }
+
+        recyclerView.addOnScrollListener(new EndlessRecyclerViewScrollListener(visibleThreshold) {
+            @Override
+            public void onLoadMore() {
+                listener.onLoadMore(recyclerView, this::setLoadFinished);
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
@@ -24,7 +79,6 @@ public class RecyclerViewBindingAdapters {
                 (ObservableRecyclerViewAdapter<T>) recyclerView.getAdapter();
 
         if (adapter == null) {
-
             adapter = new ObservableRecyclerViewAdapter<>(items, itemBinder.getLayout(), itemBinder.getBindingVariable());
             recyclerView.setAdapter(adapter);
 
