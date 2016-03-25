aeb0aa9e12265a1fbe35d339d62dee52e3630edd
mengli <dream83619@hotmail.com>
2016-03-25 10:33:20 +0800

    commit

--- app/src/main/java/com/xiaoheifamily/bookstore/binding/imageview/ImageViewBindingAdapters.java
+++ app/src/main/java/com/xiaoheifamily/bookstore/binding/imageview/ImageViewBindingAdapters.java
@@ -1,14 +1,31 @@
 package com.xiaoheifamily.bookstore.binding.imageview;
 
 import android.databinding.BindingAdapter;
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 public class ImageViewBindingAdapters {
 
-    @BindingAdapter({"bind:imageUrl"})
-    public static void loadImage(ImageView view, String url) {
-        Picasso.with(view.getContext()).load(url).into(view);
+    @BindingAdapter(value = {"imageUrl", "circle"}, requireAll = false)
+    public static void loadImage(ImageView view, String url, Boolean circle) {
+
+        if (circle == null || !circle) {
+            Glide.with(view.getContext()).load(url).into(view);
+        } else {
+            Glide.with(view.getContext()).load(url).asBitmap().centerCrop().into(new BitmapImageViewTarget(view) {
+                @Override
+                protected void setResource(Bitmap resource) {
+                    RoundedBitmapDrawable circularBitmapDrawable =
+                            RoundedBitmapDrawableFactory.create(view.getResources(), resource);
+                    circularBitmapDrawable.setCircular(true);
+                    view.setImageDrawable(circularBitmapDrawable);
+                }
+            });
+        }
     }
 }
