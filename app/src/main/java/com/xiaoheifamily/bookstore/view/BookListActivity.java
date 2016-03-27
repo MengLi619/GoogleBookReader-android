d421883aa5b4b5b33dc83ca0f56ef03ff6b73104
mengli <dream83619@hotmail.com>
2016-03-27 13:22:17 +0800

    commit

--- app/src/main/java/com/xiaoheifamily/bookstore/view/BookListActivity.java
+++ app/src/main/java/com/xiaoheifamily/bookstore/view/BookListActivity.java
@@ -46,15 +46,15 @@ public class BookListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id。ea) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
