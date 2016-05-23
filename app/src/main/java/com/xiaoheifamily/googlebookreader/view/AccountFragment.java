9b36b8cd19f0c837458f3a4daee6ec1c728b1d9d
mengli <dream83619@hotmail.com>
2016-05-23 14:41:11 +0800

    rename project

--- app/src/main/java/com/xiaoheifamily/googlebookreader/view/AccountFragment.java
+++ app/src/main/java/com/xiaoheifamily/googlebookreader/view/AccountFragment.java
@@ -1,6 +1,6 @@
 package com.xiaoheifamily.googlebookreader.view;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 
 public class AccountFragment extends Fragment {
 }
--- app/src/main/java/com/xiaoheifamily/googlebookreader/view/BookListFragment.java
+++ app/src/main/java/com/xiaoheifamily/googlebookreader/view/BookListFragment.java
@@ -1,7 +1,97 @@
 package com.xiaoheifamily.googlebookreader.view;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.xiaoheifamily.googlebookreader.App;
+import com.xiaoheifamily.googlebookreader.R;
+import com.xiaoheifamily.googlebookreader.databinding.BookListActivityBinding;
+import com.xiaoheifamily.googlebookreader.viewmodel.BookListViewModel;
+import com.xiaoheifamily.googlebookreader.widget.recyclerview.DividerItemDecoration;
+import com.xiaoheifamily.googlebookreader.widget.recyclerview.EndlessRecyclerView;
+
+import java.net.ConnectException;
 
 public class BookListFragment extends Fragment {
 
+    private BookListViewModel model;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        model = ((App) getActivity().getApplication()).getViewModelComponent().getBookListViewModel();
+
+        BookListActivityBinding binding = DataBindingUtil.inflate(inflater, R.layout.book_list_fragment, container, false);
+        binding.setModel(model);
+
+        // setup recycler view
+        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.book_list);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        // setup swipe refresh layout
+        SwipeRefreshLayout refreshLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.refresh_layout);
+
+        // bind view model events
+        model.setOnError(ex -> {
+            if (ex instanceof ConnectException) {
+                CoordinatorLayout layout = (CoordinatorLayout) getActivity().findViewById(R.id.main_layout);
+                Snackbar.make(layout, "Network Error", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        model.setOnRefreshing(() ->
+                refreshLayout.setRefreshing(true));
+
+        model.setOnRefreshFinished(() ->
+                refreshLayout.setRefreshing(false));
+
+        model.setOnLoadMoreFinished(() ->
+                ((EndlessRecyclerView) recyclerView).setLoading(false));
+
+        // start refresh
+        model.refresh();
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.main_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                model.search(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+    }
 }
